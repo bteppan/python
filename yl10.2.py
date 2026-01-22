09c8dc9c991cc944b9a4b66c7e3d1837c8d37430
bteppan <bteppan@hkhk.edu.ee>
2026-01-22 14:58:02 +0200

    gr

---
+++ yl10.2.py
@@ -0,0 +1,47 @@
+# Ülesanne 10.2
+# 13.01.2026
+import random
+
+def mangi_yht_mangu():
+    
+    arv = random.randint(1, 10)
+    katse_arv = 0
+    oigesti_arvatud = False
+
+    print("Tere! Proovi ära arvata arv vahemikus 1 ja 10.")
+
+    for _ in range(10): 
+        kasutaja_arv = int(input("Sisesta oma arv: "))
+        katse_arv += 1
+
+
+        if kasutaja_arv < arv:
+            print("Pakutud arv on liiga väike. Proovi uuesti!")
+        elif kasutaja_arv > arv:
+            print("Pakutud arv on liiga suur. Proovi uuesti!")
+        elif kasutaja_arv == arv:
+            print(f"Õige! Arvasite arvu ära {katse_arv}. katsega.")
+            break
+    
+    else:
+        print("Kahjuks ei suutnud sa õiget arvu ära arvata. Proovi uuesti!")
+
+    return katse_arv
+
+def mangu_algus():
+    koik_katsed = []  
+
+    for i in range(10):  
+        katse_arv = mangi_yht_mangu()
+        koik_katsed.append(katse_arv)
+
+        uuesti = input("Kas soovid uuesti mängida? (jah/ei): ").strip().lower()
+        if uuesti != "jah":
+            print("\Mängu tulemused:")
+            for j, katse in enumerate(kõik_katsed, 1):
+                print(f"Mäng {j}: Arvasite ära {katse}. katsega.")
+            print("Aitäh mängimast!")
+            break
+
+    print()  
+mangu_algus()
