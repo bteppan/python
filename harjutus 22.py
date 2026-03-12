22a8b11a872199baf1a1eb03246bb305911a2dd9
bteppan <bteppan@hkhk.edu.ee>
2026-03-12 13:23:28 +0200

    67

--- harjutus 22.py
+++ harjutus 22.py
@@ -7,3 +7,5 @@ print(kp.strftime("%d.%m %Y, %H:%M:%S"))
 sp = datetime.datetime(2009,11,15)
 vanus_paevades = kp - sp
 print(f"vanus päevades: {vanus_paevades}")
+vanus_aastates = vanus_paevades // 365
+print(f"Vanus aastates: {vanus_aastates}")
