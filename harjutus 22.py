ad44a8f606cf45e103caf2ea7b9d54ce12a62f08
bteppan <bteppan@hkhk.edu.ee>
2026-03-12 13:27:14 +0200

    67

--- harjutus 22.py
+++ harjutus 22.py
@@ -4,8 +4,15 @@ kp = datetime.datetime.now()
 print(kp)
 
 print(kp.strftime("%d.%m %Y, %H:%M:%S"))
+
 sp = datetime.datetime(2009,11,15)
 vanus_paevades = kp - sp
 print(f"vanus päevades: {vanus_paevades}")
-vanus_aastates = vanus_paevades // 365
+
+vanus_aastates = vanus_paevades.days // 365
 print(f"Vanus aastates: {vanus_aastates}")
+
+if vanus_aastates%5==0:
+    print("Sul on juubel!")
+else:
+    print("sul ei ole juubel!!")
